1f953602b46005874c015675a11a715c9005960d
jav974 <jeremyvienne@gmail.com>
2022-02-21 14:43:24 +0100

    first commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Controller;
+
+use App\Handler\YtDlpHandler;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    private YtDlpHandler $ytDlpHandler;
+
+    public function __construct(YtDlpHandler $ytDlpHandler)
+    {
+        $this->ytDlpHandler = $ytDlpHandler;
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/", name="home")
+     */
+    public function index(Request $request): Response
+    {
+        $form = $this->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $url = $form->getData()['url'];
+
+            if ($form->get('submitVideo')->isClicked()) {
+                return $this->downloadVideo($url);
+            } elseif ($form->get('submitAudio')->isClicked()) {
+                return $this->downloadAudio($url);
+            }
+        }
+
+        return $this->render('Default/index.html.twig', ['form' => $form->createView()]);
+    }
+
+    private function getForm(): FormInterface
+    {
+        return $this->createFormBuilder(null, ['method' => 'GET'])
+            ->add('url', UrlType::class, ['label' => false, 'attr' => ['placeholder' => 'URL']])
+            ->add('submitVideo', SubmitType::class, ['label' => 'Vidéo', 'attr' => ['class' => 'btn-warning']])
+            ->add('submitAudio', SubmitType::class, ['label' => 'Audio', 'attr' => ['class' => 'btn-primary']])
+            ->getForm()
+        ;
+    }
+
+    private function downloadVideo(string $url): Response
+    {
+        $file = $this->ytDlpHandler->extractVideo($url);
+
+        return $this->file($file, basename($file));
+    }
+
+    private function downloadAudio(string $url): Response
+    {
+        $file = $this->ytDlpHandler->extractAudio($url);
+
+        return $this->file($file, basename($file));
+    }
+}
---
+++ src/Handler/YtDlpHandler.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Handler;
+
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+
+class YtDlpHandler
+{
+    private string $ytDlpPath;
+    private string $ffmpegPath;
+    private string $outputPath;
+
+    public function __construct(string $ytDlpPath, string $ffmpegPath, string $outputPath)
+    {
+        $this->ytDlpPath = $ytDlpPath;
+        $this->ffmpegPath = $ffmpegPath;
+        $this->outputPath = $outputPath;
+    }
+
+    public function extractVideo(string $url): string
+    {
+        $filename = $this->getFilename($url);
+        $process = new Process([$this->ytDlpPath, $url, '-o', "$this->outputPath/$filename.%(ext)s"]);
+        $outputFilename = '';
+
+        $process->run(function(string $type, string $line) use(&$outputFilename) {
+            if ($type !== 'out') {
+                return ;
+            }
+
+            if (preg_match('/\[download] (?<dest>.+) has already been downloaded/', $line, $match)) {
+                $outputFilename = $match['dest'];
+            }
+
+            if (preg_match('/\[download] Destination: (?<dest>.+)/', $line, $match)) {
+                $outputFilename = $match['dest'];
+            }
+        });
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return $outputFilename;
+    }
+
+    public function extractAudio(string $url): string
+    {
+        $filename = $this->getFilename($url);
+        $process = new Process([$this->ytDlpPath, "-x", $url, "-o", "$this->outputPath/$filename.%(ext)s", '--ffmpeg-location', $this->ffmpegPath]);
+        $outputFilename = '';
+
+        $process->run(function(string $type, string $line) use(&$outputFilename) {
+            if ($type !== 'out') {
+                return ;
+            }
+
+            if (preg_match('/\[ExtractAudio] Destination: (?<dest>.+)/', $line, $match)) {
+                $outputFilename = $match['dest'];
+            }
+        });
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return $outputFilename;
+    }
+
+    public function getMetadata(string $url)
+    {
+        $process = new Process([$this->ytDlpPath,  "-J", "$url"]);
+        $process->run();
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        return json_decode($process->getOutput(), true);
+    }
+
+    public function getFilename(string $url): string
+    {
+        $metadata = $this->getMetadata($url);
+
+        return $metadata['title'];
+    }
+}
